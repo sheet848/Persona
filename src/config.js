0cbb33e7b1dc157fb0d2e7f8545da9c3e6414559
Sheetal Naik <sheetalnaik848@gmail.com>
2025-03-02 21:09:37 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 export const metaData = {
   baseUrl: "https://astrofolio-astro.vercel.app/",
-  title: "Astrofolio",
-  name: "Vikas",
+  title: "Persona",
+  name: "Sheetal",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "A clean, fast, and lightweight personal portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/vikas5914",
-  github: "https://github.com/vikas5914/Astrofolio",
+  /*twitter: "https://x.com/vikas5914",*/
+  github: "https://github.com/sheet848/",
   instagram: "https://www.instagram.com/",
   linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  email: "mailto:sheetalnaik848@gmail.com",
 };
